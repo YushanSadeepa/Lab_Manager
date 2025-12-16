8465a2206a434af6c26d4b2ddd2bfab279b819ae
YushanSadeepa <yushanaththanayake@gmail.com>
2025-12-16 12:08:40 +0530

    Add report submission details to database and prepare for table display

--- Lab_Coordinator/Properties/AssemblyInfo.cs
+++ Lab_Coordinator/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab_Coordinator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Lab Manager")]
+[assembly: AssemblyDescription("A Windows Forms application for managing university laboratory schedules, student registration, attendance, and lab report submissions.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab_Coordinator")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Y.S.Aththanayake")]
+[assembly: AssemblyProduct("Lab Manager")]
+[assembly: AssemblyCopyright("© 2025 Yushan Sadeepa. All rights reserved.")]
+[assembly: AssemblyTrademark("LabManager™")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("43420179-0101-4395-84c5-7ae16873dba1")]
